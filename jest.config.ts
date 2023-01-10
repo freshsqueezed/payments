e81d004debfe20bd7c203a985a49749a9d6fa314
Matthew Gordon <m.gordon5525@gmail.com>
2023-01-09 20:56:50 -0700

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['src'],
+  transform: {
+    ts: [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json',
+      },
+    ],
+  },
+  testRegex: '/__tests__/.*.test.ts$',
+  verbose: true,
+};
+
+export default config;
